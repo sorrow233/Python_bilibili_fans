040c8c3913d9b4441831797592cdbd91b7a2afbd
KangHu <60466986+sorrowful2233@users.noreply.github.com>
2023-05-23 14:34:41 +0800

    Update User_info.py

--- User_info.py
+++ User_info.py
@@ -1,19 +1,14 @@
 import requests
 
-with open('links.txt', 'r') as file:
-    for url in file:
-        url = url.strip()  # 去除换行符和空格
+url = 'https://api.vc.bilibili.com/account/v1/user/cards'
+cookies = {'SESSDATA': '7814cba3%2C1700373239%2C7e695%2A51'}
+params = {'uids': '34814137,2,3'}
 
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        }
-        response = requests.get(url,headers=headers)
+response = requests.get(url, params=params, cookies=cookies)
 
-        if response.status_code == 200:
-            html_data = response.text
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print("请求失败")
 
-            with open('output.html', 'w', encoding='utf-8') as file:
-                file.write(html_data)
-                print("网页数据保存成功")
-        else:
-            print("无法访问网页")
